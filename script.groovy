6686bf771e33d4e60bec65b1090d414b51c52a27
Deepakkachole <deepakkachole@yahoo.com>
2021-12-07 13:54:31 +0000

    File from Git

--- script.groovy
+++ script.groovy
@@ -1,10 +1,14 @@
-def testApp(){
-echo "Testing the App"
-}
-def buildApp(){
-echo "Building the App"
-}
-def deployApp(){
-echo "Deploying the App"
-}
+def buildApp() {
+    echo 'building the application...'
+} 
+
+def testApp() {
+    echo 'testing the application...'
+} 
+
+def deployApp() {
+    echo 'deplying the application...'
+    echo "deploying version ${params.VERSION}"
+} 
+
 return this
